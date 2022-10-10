ff9c3ee16377a9169307f66ffef59b38900d4d3d
Yshmeel <me@yshmeel.dev>
2022-10-10 22:22:10 +0500

    fox

--- C4 (variant 2)/index.js
+++ C4 (variant 2)/index.js
@@ -2,7 +2,7 @@ const renderButton = document.querySelector('#render-button');
 
 renderButton.addEventListener('click', function() {
     window.document.designMode = "On";
-    window.document.execCommand("hiliteColor", 'red', "yellow", 'red');
+    window.document.execCommand("hiliteColor", false, "yellow");
     window.document.designMode = "Off";
 
     document.querySelectorAll('span').forEach((a) => {
